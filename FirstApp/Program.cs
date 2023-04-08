7b834be0d1c10e7ddbed5f6fd5234003d360ca8b
StacyDeveloper <129184938+StacyDeveloper@users.noreply.github.com>
2023-04-08 17:41:47 +0300

    Добавьте файлы проекта.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string myName = "Anastasia";
+        byte age = 28;
+        bool DoIHaveAPet = true;
+        double size = 36.5;
+
+        Console.WriteLine("My name is " + myName);
+        Console.WriteLine("My age is " + age);
+        Console.WriteLine("Do I have a pet? " + DoIHaveAPet);
+        Console.WriteLine("My shoe size is " + size);
+
+        Console.ReadKey();
+    }
+}
