dda85d18eaef7f85539a9555b94a46115ec60883
StacyDeveloper <129184938+StacyDeveloper@users.noreply.github.com>
2023-04-20 18:56:05 +0300

    Модифицировала свою программу для ввода личной информации пользователя так, чтобы данные записывались в кортеж anketa.

--- UserSurvey/Program.cs
+++ UserSurvey/Program.cs
@@ -1,11 +1,18 @@
-﻿Console.Write("Введите ваше имя ");
-string name = Console.ReadLine();
-Console.Write("Введите ваш возраст ");
-byte age = byte.Parse(Console.ReadLine());
-Console.Write("Введите ваш рост ");
-int height = Convert.ToInt32(Console.ReadLine());
-Console.Write("Укажите вашу дату рождения ");
+﻿(string name, int age) anketa;
+Console.WriteLine("Мое имя: Анастасия");
+Console.WriteLine("Мой возраст: 28");
+Console.Write("Введите ваше имя: \n");
+anketa.name = Console.ReadLine();
+Console.Write("Введите ваш возраст: \n");
+anketa.age = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите ваш рост: \n");
+int height = int.Parse(Console.ReadLine());
+Console.Write("Укажите вашу дату рождения: \n");
 DateOnly date = DateOnly.Parse(Console.ReadLine());
-Console.Write("Давайте проверим информацию: ");
-Console.WriteLine("Вас зовут {0}, Вам {1} лет, Ваш рост {2} см, Ваша дата рождения {3}", name, age, height, date);
+Console.Write("Давайте проверим информацию: \n");
+Console.WriteLine("Вас зовут {0}, Вам {1} лет, Ваш рост {2} см, Ваша дата рождения {3}", anketa.name, anketa.age, height, date);
 Console.ReadKey();
+
+
+
+
